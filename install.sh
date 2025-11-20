aac384e70a4df25b2e58af7daebc8b2b63e37b95
harryedgew <harryedgew@gmail.com>
2025-11-20 23:13:06 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
 sudo add-apt-repository ppa:papirus/papirus
 sudo add-apt-repository multiverse
 sudo apt-get update
