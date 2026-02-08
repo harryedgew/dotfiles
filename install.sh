58689cdb51addb0599846122f5193de225a7edd5
harryedgew <harryedgew@gmail.com>
2026-02-08 16:54:33 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-releas
 xargs sudo dnf install -y < packages.txt
 
 #Zsh plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/zsh-autosuggestions
 sudo mkdir /usr/share/zsh/plugins
 sudo mv ~/zsh-autosuggestions ~/zsh-syntax-highlighting /usr/share/zsh/plugins
