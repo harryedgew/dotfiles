3bd2b40e07ec0b3a815d039d7f61846c1a16424c
harryedgew <harryedgew@gmail.com>
2026-02-18 15:12:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo dnf up -y
 
-#Steam
+# Steam
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
 
@@ -8,10 +8,10 @@ sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
 sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
-#Install packages
+# Install packages
 xargs sudo dnf install -y < packages.txt
 
-#Zsh plugins
+# Zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/zsh-autosuggestions
 sudo mkdir /usr/share/zsh/plugins
