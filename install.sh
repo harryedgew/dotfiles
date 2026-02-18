d073370c1c84970f9dd25f8d3c09fd1392bac6e1
Harry Edgeworth <harryedgew@gmail.com>
2026-02-18 16:43:52 +0000

    Install script now includes install for vim-plug

--- install.sh
+++ install.sh
@@ -24,3 +24,7 @@ chsh -s $(which zsh)
 # Move configs
 mv ~/dotfiles/.config/nvim ~/.config/
 mv ~/dotfiles/.zshrc ~/
+
+# vim-plug Install
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
